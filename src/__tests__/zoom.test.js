7ae7393ff25fc1d7eb61cd98ae4981ecde0fba2b
jad <jadesrochers@gmail.com>
2019-10-29 15:09:35 -0400

    modify tests to match changes in useZoom

--- src/__tests__/zoom.test.js
+++ src/__tests__/zoom.test.js
@@ -28,14 +28,14 @@ describe('useZoom hook tests', () => {
     expect(hook.scale).toEqual(1.4)
     expect(hook.shiftxpct).toEqual("-2%")
     expect(hook.shiftypct).toEqual("-2%")
-    hook.pan({width: 100, height: 50}, 10, 5, true)
+    hook.pan(10, 5, true)
 
     hook = wrapper.find('div').props().hook
     expect(hook.shiftxpct).toEqual("5%")
     expect(hook.shiftypct).toEqual("1%")
 
     // The offsetlimit is 15% at scale(1.4), and this will run into that.
-    hook.pan({width: 100, height: 50}, 20, 10, false)
+    hook.pan(20, 10, false)
     hook = wrapper.find('div').props().hook
     expect(hook.shiftxpct).toEqual("12%")
     expect(hook.shiftypct).toEqual("5%")
@@ -43,7 +43,7 @@ describe('useZoom hook tests', () => {
     // Since the last action set the end (false) this will
     // move from that end point.
     hook = wrapper.find('div').props().hook
-    hook.pan({width: 100, height: 50}, -10, -5, true)
+    hook.pan(-10, -5, true)
 
     hook = wrapper.find('div').props().hook
     expect(hook.shiftxpct).toEqual("4%")
